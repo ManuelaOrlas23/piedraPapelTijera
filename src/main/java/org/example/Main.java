a1b08a435b1e420dcc9e4a3cef204cf4522eab33
ManuelaOrlas23 <manurengifo205@gmail.com>
2024-02-05 09:45:33 -0500

    Se crean cambios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,41 +8,51 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                Random random = new Random();
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+                String[] opciones = {"piedra", "papel", "tijera"};
 
-        String[] opciones = {"piedra", "papel", "tijera"};
+                int victorias = 0;
+                int derrotas = 0;
+                int empates = 0;
 
-        while (true) {
-            System.out.println("Elige: piedra, papel o tijera (o 'salir' para terminar): ");
-            String eleccionJugador = scanner.nextLine().toLowerCase();
+                while (true) {
+                    System.out.println("Elige: piedra, papel o tijera (o 'salir' para terminar): ");
+                    String eleccionJugador = scanner.nextLine().toLowerCase();
 
-            if (eleccionJugador.equals("salir")) {
-                System.out.println("¡Hasta luego!");
-                break;
-            }
+                    if (eleccionJugador.equals("salir")) {
+                        System.out.println("¡Hasta luego!");
+                        break;
+                    }
 
-            if (!eleccionJugador.equals("piedra") && !eleccionJugador.equals("papel") && !eleccionJugador.equals("tijera")) {
-                System.out.println("Elección no válida. Por favor, elige piedra, papel o tijera.");
-                continue;
-            }
+                    if (!eleccionJugador.equals("piedra") && !eleccionJugador.equals("papel") && !eleccionJugador.equals("tijera")) {
+                        System.out.println("Elección no válida. Por favor, elige piedra, papel o tijera.");
+                        continue;
+                    }
 
-            int indiceComputadora = random.nextInt(3);
-            String eleccionComputadora = opciones[indiceComputadora];
+                    int indiceComputadora = random.nextInt(3);
+                    String eleccionComputadora = opciones[indiceComputadora];
 
-            System.out.println("La computadora eligió: " + eleccionComputadora);
+                    System.out.println("La computadora eligió: " + eleccionComputadora);
 
-            if (eleccionJugador.equals(eleccionComputadora)) {
-                System.out.println("¡Empate!");
-            } else if ((eleccionJugador.equals("piedra") && eleccionComputadora.equals("tijera")) ||
-                    (eleccionJugador.equals("papel") && eleccionComputadora.equals("piedra")) ||
-                    (eleccionJugador.equals("tijera") && eleccionComputadora.equals("papel"))) {
-                System.out.println("¡Ganaste!");
-            } else {
-                System.out.println("¡Perdiste!");
-            }
-        }
+                    if (eleccionJugador.equals(eleccionComputadora)) {
+                        System.out.println("¡Empate!");
+                        empates++;
+                    } else if ((eleccionJugador.equals("piedra") && eleccionComputadora.equals("tijera")) ||
+                            (eleccionJugador.equals("papel") && eleccionComputadora.equals("piedra")) ||
+                            (eleccionJugador.equals("tijera") && eleccionComputadora.equals("papel"))) {
+                        System.out.println("¡Ganaste!");
+                        victorias++;
+                    } else {
+                        System.out.println("¡Perdiste!");
+                        derrotas++;
+                    }
+                }
+
+                System.out.println("Resultados: Victorias: " + victorias + ", Derrotas: " + derrotas + ", Empates: " + empates);
+
+                scanner.close();
 
 
     }
